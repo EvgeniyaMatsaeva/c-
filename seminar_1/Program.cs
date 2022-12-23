ebf7c15500ca687f1bef2d7bf84a1a419e8a021b
Evgeniya <evgeniya26.12@mail.ru>
2022-12-24 01:31:03 +0300

    dont get it

--- seminar_1/Program.cs
+++ seminar_1/Program.cs
@@ -23,33 +23,55 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
-Console.WriteLine("введите число");
-int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("введите число");
-int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число");
+// int number2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("введите число");
-int number3 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число");
+// int number3 = Convert.ToInt32(Console.ReadLine());
 
-if(number1>number2);
-    if(number1>number3)
-    {
-        Console.WriteLine( "максимальное число=  number1");
-    }
+// if(number1>number2);
+//     if(number1>number3)
+//     {
+//         Console.WriteLine( "максимальное число=  number1");
+//     }
 
-    else
-    {
-        Console.WriteLine("максимальное число = number3");
-    }
+//     else
+//     {
+//         Console.WriteLine("максимальное число = number3");
+//     }
 
-if (number2 > number3)
-{
-    Console.WriteLine ("максимальное число = number2");
-    }
+// if (number2 > number3)
+// {
+//     Console.WriteLine ("максимальное число = number2");
+//     }
 
-    
+//     else { Console.WriteLine("максимальное число = number3");
 
-else { Console.WriteLine("максимальное число = number3");
+// };
 
-};
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.WriteLine("введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if(number%2==0);
+//  {
+//     Console.WriteLine("четное");
+//  };
+
+//   else
+//  {
+//   Console.WriteLine("нечётное");  
+//  }
+
+//  Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
