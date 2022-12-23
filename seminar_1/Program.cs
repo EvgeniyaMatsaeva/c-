e83f3936efb602d097ffb7af188bb108ff784414
Evgeniya <evgeniya26.12@mail.ru>
2022-12-23 23:50:38 +0300

    first

--- seminar_1/Program.cs
+++ seminar_1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("введите число");
+int number1=Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine("введите число");
+int number2=Convert.ToInt32 (Console.ReadLine());
+
+if (number1 > number2);
+{
+    Console.WriteLine (number1);
+}
+
+if
+ (number2 > number1)
+{
+    Console.WriteLine (number2);
+};
